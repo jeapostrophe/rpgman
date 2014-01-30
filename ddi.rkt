fb6776c5018708a15b908352b9e4ca11ee123fe4
Jay McCarthy <jay.mccarthy@gmail.com>
2014-01-29 21:24:31 -0700

    verify counts

--- ddi.rkt
+++ ddi.rkt
@@ -1,5 +1,9 @@
 #lang racket/base
-(require net/url)
+(require net/url
+         racket/match
+         racket/list
+         xml/path
+         xml)
 
 (define tabs
   '("Race"
@@ -23,6 +27,9 @@
 (define all-url-s
   "http://wizards.com/dndinsider/compendium/CompendiumSearch.asmx/ViewAll?&Tab=~a")
 
+(define (se-path*/list+tags p xe)
+  (filter pair? (se-path*/list p xe)))
+
 (module+ main
   (require racket/runtime-path
            racket/file
@@ -32,9 +39,31 @@
   (make-directory* dest-dir)
 
   (for ([t (in-list tabs)])
+    (printf "~a\n" t)
+
     (define tab.xml (build-path dest-dir (format "~a.xml" t)))
     (unless (file-exists? tab.xml)
       (define tab-url-s (format all-url-s t))
       (define tab-url (string->url tab-url-s))
       (define-values (st hd data) (http-sendrecv/url tab-url))
-      (display-to-file (port->bytes data) tab.xml))))
+      (display-to-file (port->bytes data) tab.xml))
+
+    (define xs (file->string tab.xml))
+    (define xe (parameterize ([collapse-whitespace #t]
+                              [xexpr-drop-empty-attributes #t])
+                 (string->xexpr xs)))
+
+    (match-define
+     `((Table ,(== t)) (Total ,(app string->number expected-total)))
+     (se-path*/list+tags '(Data Totals Tab) xe))
+
+    (define rs
+      (se-path*/list+tags '(Data Results) xe))
+    (define actual-total
+      (length rs))
+
+    (unless (= expected-total actual-total)
+      (error 'ddi "~a: count mismatch: ~e vs ~e"
+             t expected-total actual-total))
+
+    (printf "\t~a\n" actual-total)))
