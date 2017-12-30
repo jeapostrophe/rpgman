6baf179ea852a60adc9931a14c362f7b787bbc8f
Jay McCarthy <jay.mccarthy@gmail.com>
2017-12-30 16:59:47 -0500

    up

--- ddi.rkt
+++ ddi.rkt
@@ -9,8 +9,7 @@
          racket/string
          racket/set
          db
-         sxml
-         text-table)
+         sxml)
 (define-runtime-path ddi "ddi")
 
 (define (file->sxml f)
@@ -131,50 +130,108 @@
      [35 47000 11750 94000 235000])))
 
 (define (plan-encounters! player-count)
-  (displayln
-   (table->string
-    (for/fold ([prev-xp 0]
-               [tbl (list (list "Level" "XP (Player)"
-                                "XP (Party)"
-                                "#" "Monsters"))]
-               #:result
-               tbl)
-              ([level (in-range 1 30)]
-               [xxx (in-range 5)])
-      (match-define (list required-xp) (hash-ref TotalXP-PHB-pg29 (+ 1 level)))
-      (define xp-left (- required-xp prev-xp))
+  (define level-sep (~a (make-string 80 #\-) "\n"))
+  (define (col-sep v)
+    (define s (~a v))
+    (~a s (make-string (- 10 (string-length s)) #\space)))
+  (display (~a (col-sep "Level") "\n"))
+  (display level-sep)
 
-      (define (standard-xp level)
-        (first (hash-ref ExperiencePointRewards-DMG-pg56 level)))
-      (define per-encounter-hard
-        (ceiling
-         (* (standard-xp (+ level 4))
-            player-count)))
+  (for/fold ([prev-xp 0])
+            ([level (in-range 1 30)]
+             [xxx (in-range 5)])
+    (match-define (list required-xp) (hash-ref TotalXP-PHB-pg29 (+ 1 level)))
+    (define xp-left (- required-xp prev-xp))
 
-      (define xp-left-party (* player-count xp-left))
+    (define (standard-xp level)
+      (first (hash-ref ExperiencePointRewards-DMG-pg56 level)))
+    (define per-encounter-hard
+      (ceiling
+       (* (standard-xp (+ level 4))
+          player-count)))
 
-      (define (dole-encounters xp es)
-        (define s (sum es))
-        (unless (= s 100)
-          (error 'dole-encounters "Encounters do not total 100%, s=~e" s))
-        (for/list ([e (in-list es)])
-          (ceiling (* xp (/ e 100)))))
-      (define xp-encounters (dole-encounters xp-left-party '(15 20 15 25 25)))
+    (define xp-left-party (* player-count xp-left))
 
-      (define how-many-encounters
-        (ceiling (/ xp-left-party per-encounter-hard)))
-      (define encounters
-        (for/list ([i (in-naturals 1)]
-                   [e (in-range how-many-encounters)])
-          (list "" "" "" i "XXX")))
+    ;; DMG 58 -- These are described there.
 
-      (values
-       required-xp
-       (append tbl
-               (cons (list level xp-left xp-left-party "" "")
-                     encounters)))))))
+    ;; XXX Incorporate the "simple substitutions", in particular it says
+    ;; 4 Minions = 1 Standard
+    ;; 1 Elite   = 2 Standard
+    ;; 1 Solor   = 5 Standard
+    (define encounter-templates
+      (list
+       (vector "Battlefield Control"
+               (list (vector 1 'Controller (+ level 5))
+                     (vector 5 'Skirmisher (+ level 1))))
+       (vector "Commander and Troops"
+               (list (vector 1 '(Controller Soldier) (+ level 6))
+                     (vector 3 '(Brute Soldier) (+ level 1))
+                     (vector 2 'Artillery (+ level 1))))
+       (vector "Dragon's Den"
+               (list (vector 1 'Solo (+ level 3))))
+       (vector "Dragon's Den"
+               (list (vector 1 'Solo (+ level 1))
+                     (vector 1 'Elite (+ level 0))))
+       (vector "Double Line"
+               (list (vector 3 '(Brute Soldier) (+ level 2))
+                     (vector 1 'Controller (+ level 4))
+                     (vector 1 '(Artillery Lurker) (+ level 4))))
+       (vector "Double Line"
+               (list (vector 3 '(Brute Soldier) level)
+                     (vector 2 'Artillery (+ level 1))
+                     (vector 1 'Controller (+ level 2))
+                     (vector 1 'Lurker (+ level 2))))
+       (vector "Wolf Pack" (list (vector 3 'Skirmisher (+ level 7))))
+       (vector "Wolf Pack" (list (vector 4 'Skirmisher (+ level 5))))
+       (vector "Wolf Pack" (list (vector 6 'Skirmisher (+ level 2))))))
 
-;; DMG 126-- has a table of magic item rewards
+    (define (monster-xp what level)
+      (match-define (list standard minion elite solo)
+        (hash-ref ExperiencePointRewards-DMG-pg56 level))
+      (match what
+        ['Solo solo]
+        ['Elite elite]
+        [(or 'Skirmisher 'Controller 'Artillery 'Soldier 'Brute 'Lurker)
+         standard]
+        [(? list?)
+         (apply max
+                (for/list ([w (in-list what)])
+                  (monster-xp w level)))]))
+    (define (template-xp t)
+      (for/sum ([t (in-list t)])
+        (match-define (vector how-many what level) t)
+        (* how-many (monster-xp what level))))
+
+    (display (~a (col-sep level) xp-left-party "\n"))
+    (define how-many-encounters
+      (ceiling (/ xp-left-party per-encounter-hard)))
+    (for/fold ([xp-so-far 0])
+              ([i (in-naturals 1)]
+               [e (in-range how-many-encounters)]
+               [t-id*mobs (in-list (shuffle encounter-templates))])
+      (match-define (vector template-id mobs) t-id*mobs)
+      (define this-xp (template-xp mobs))
+      (define xp-after-this (+ this-xp xp-so-far))
+      (display "\n")
+      (display (~a (col-sep "") (col-sep xp-after-this)
+                   "Reward: " "Magic-" (+ level e) "\n"))
+      (display (~a (col-sep "") (col-sep "") "= "template-id "\n"))
+      ()
+      (display (~a (col-sep "") (col-sep "") mobs "\n"))
+      ;; DMG 126 -- has a table of magic item rewards that
+      ;; is like this, except that it starts at (+ level e
+      ;; 1) and only gives four per level.
+      (display (~a (col-sep "") (col-sep "") this-xp "\n"))
+
+      xp-after-this)
+    (display level-sep)
+
+    ;; xxx generate a web page?
+
+    required-xp)
+
+  (void))
+
 
 (module+ main
   (monster-initialize!)
